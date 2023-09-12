ebe3f7334d3fa6c64f68ae1afade7d705e25b902
martiap <martiap93@gmail.com>
2023-09-12 12:06:14 +0200

    Add app and index

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import createDebug from 'debug';
+import 'dotenv/config';
+import { createServer } from 'http';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
+
+const debug = createDebug('Users:Index');
+const PORT = process.env.PORT || 3000;
+
+const server = createServer(app);
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('Connected to DB:', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    server.emit('error', error);
+  });
+
+server.on('listening', () => {
+  console.log(`Lisening on port ${PORT}`);
+});
+
+server.on('error', (error) => {
+  console.log(`Error ${error.message}`);
+});
