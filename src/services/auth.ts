d376ca5efbaa3c1d5becd611ae88ef223abe04c5
martiap <martiap93@gmail.com>
2023-09-12 20:50:43 +0200

    Add user controller and auth and pass tests

---
+++ src/services/auth.ts
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt';
+import 'dotenv/config';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { HttpError } from '../types/http.error.js';
+
+export type TokenPayload = JwtPayload & {
+  id: string;
+  userName: string;
+};
+export class Auth {
+  static passwd = process.env.TOKEN_PASSWD!;
+  static hash(passwd: string) {
+    const saltRounds = 10;
+    return bcrypt.hash(passwd, saltRounds);
+  }
+
+  static compare(passwd: string, hashPasswd: string) {
+    const a = bcrypt.compare(passwd, hashPasswd);
+    return a;
+  }
+
+  static signToken(payload: TokenPayload): string {
+    return jwt.sign(payload, Auth.passwd);
+  }
+
+  static verifyTokenGettingPayload(token: string) {
+    try {
+      const result = jwt.verify(token, Auth.passwd);
+      if (typeof result === 'string') {
+        throw new HttpError(498, 'Not valid token', result);
+      }
+
+      return result as TokenPayload;
+    } catch (error) {
+      throw new HttpError(498, 'Not valid token', (error as Error).message);
+    }
+  }
+}
