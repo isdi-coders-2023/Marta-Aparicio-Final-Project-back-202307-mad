291c5142f2e14f776cd4e4da516f0463eff50712
martiap <martiap93@gmail.com>
2023-09-13 14:49:54 +0200

    Change uri to connect with mongo

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,40 @@ import 'dotenv/config';
 import { createServer } from 'http';
 import { app } from './app.js';
 import { dbConnect } from './db/db.connect.js';
-
-const debug = createDebug('Users:Index');
+const debug = createDebug('Proyecto-final');
 const PORT = process.env.PORT ?? 3000;
 
 const server = createServer(app);
+
 dbConnect()
   .then((mongoose) => {
     server.listen(PORT);
     debug('Connected to DB:', mongoose.connection.db.databaseName);
+    console.log('Connected to DB:', mongoose.connection.db.databaseName);
   })
   .catch((error) => {
     server.emit('error', error);
   });
 
 server.on('listening', () => {
-  console.log(`Lisening on port ${PORT}`);
+  const addressInfo = server.address();
+  if (addressInfo === null) {
+    server.emit('error', new Error('Invalid network address'));
+    return;
+  }
+
+  let bind: string;
+  if (typeof addressInfo === 'string') {
+    bind = 'pipe ' + addressInfo;
+  } else {
+    bind =
+      addressInfo.address === '::'
+        ? `http://localhost:${addressInfo?.port}`
+        : `port ${addressInfo?.port}`;
+  }
+
+  debug('Listening');
+  console.log(`Listening on ${bind}`);
 });
 
 server.on('error', (error) => {
